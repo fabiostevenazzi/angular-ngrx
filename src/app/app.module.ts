6efb7f7955de0330eb779d5706f4c6577e331462
fabiostevenazzi <fabio.stevenazzi@altran.it>
2020-10-29 16:02:13 +0100

    cp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { StoreModule } from '@ngrx/store';
-import { counterReducer } from './counter.reducer';
+import { NgModule } from '@angular/core';
+
 import { AppComponent } from './app.component';
 
+import { StoreModule } from '@ngrx/store';
+import { counterReducer } from './counter.reducer';
+import { MyCounterComponent } from './my-counter/my-counter.component';
+
 @NgModule({
+  declarations: [AppComponent, MyCounterComponent],
   imports: [BrowserModule, StoreModule.forRoot({ count: counterReducer })],
-  declarations: [AppComponent],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
-
-
-/*
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://github.com/ngrx/platform
-*/
